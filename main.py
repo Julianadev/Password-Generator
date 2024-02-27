2a9768b6de0441ca404d8d66ee48333abd2f4f63
julianadev <juramos266@gmail.com>
2024-02-27 01:11:15 -0300

    alter to PySimpleGUI

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import secrets
 import string
+import PySimpleGUI as sg
 
 
-print('============================ Gerador de Senhas ================================')
+def generate_password(length):
+    caracteres = string.ascii_letters + string.punctuation + string.digits
+    senha = ''.join(secrets.choice(caracteres) for i in range(length))
+    return senha
 
-escolha_usuario = int(input('Digite qual o tamanho que você quer que sua senha seja: '))
+layout = [
+    [sg.Text('Digite o tamanho da senha que você deseja:')],
+    [sg.Input(key='-LENGTH-', size=(20,1))],
+    [sg.Button('Gerar Senha'), sg.Button('Sair')],
+    [sg.Text(size=(40, 1), key='-OUTPUT-')]
+]
 
-caracteres = string.ascii_letters + string.punctuation + string.digits
-senha = ''.join(secrets.choice(caracteres) for i in range(escolha_usuario))
+window = sg.Window('Gerador de Senha', layout)
 
-print(senha)
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == 'Sair':
+        break
+    elif event == 'Gerar Senha':
+        try:
+            length = int(values['-LENGTH-'])
+            password = generate_password(length)
+            window['-OUTPUT-'].update(password)
+        except ValueError:
+            window['-OUTPUT-'].update('Por favor, insira um número inteiro válido.')
+
+window.close()
